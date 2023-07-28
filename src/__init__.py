891c1d56de3ecdd3ce4ae96e9fab41441e0de2b2
mova <mova@users.noreply.github.com>
2023-07-28 16:13:39 +0200

    venv setup exectuable

--- src/__init__.py
+++ src/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """mova"""
 __email__ = "mova@users.noreply.github.com"
-__version__ = "0.1.0"
+__version__ = '0.0.1'  # fmt: skip
--- venv_setup.sh
+++ venv_setup.sh
@@ -2,7 +2,7 @@
 set -xe
 python -m venv venv
 source venv/bin/activate
-
+pip install --upgrade pip flit
 if [[ ${HOSTNAME} == max-*desy.de ]]; then
     source ${MODULESHOME}/init/bash
     module load maxwell gcc/9.3
@@ -10,3 +10,4 @@ fi
 
 flit install --symlink
 pre-commit install
+echo "Run `source venv/bin/activate` to activate the enviroment"
