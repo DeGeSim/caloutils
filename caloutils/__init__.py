1bbb40b32aeb8ecd161ea6450ce465df7c7091a3
mova <mova@users.noreply.github.com>
2023-07-31 14:46:54 +0200

    Bump version: 0.0.3 → 0.0.4

--- caloutils/__init__.py
+++ caloutils/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """mova"""
 __email__ = "mova@users.noreply.github.com"
-__version__ = '0.0.3'  # fmt: skip
+__version__ = '0.0.4'  # fmt: skip
 
 # from processing.batch_to_Exyz import batch_to_Exyz
 # from processing.pca import fpc_from_batch
