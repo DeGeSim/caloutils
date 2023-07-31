cf1beb7b85134a353262ab3b4c71a9247cc81a07
kaechb <benno.kaech@hotmail.com>
2023-07-31 12:22:11 +0200

    added calorimeter

--- caloutils/__init__.py
+++ caloutils/__init__.py
@@ -4,13 +4,8 @@ __author__ = """mova"""
 __email__ = "mova@users.noreply.github.com"
 __version__ = '0.0.3'  # fmt: skip
 
-from preprocessing.convcoord import batch_to_Exyz
-from preprocessing.pca import fpc_from_batch
-from preprocessing.voxelize import sum_duplicate_hits, voxelize
-from torch_geometric.data import Batch, Data
-from torch_geometric.nn.pool import (
-    global_add_pool,
-    global_max_pool,
-    global_mean_pool,
-)
-from torch_scatter import scatter_std
+# from processing.batch_to_Exyz import batch_to_Exyz
+# from processing.pca import fpc_from_batch
+# from processing.voxelize import sum_duplicate_hits, voxelize
+# from torch_geometric.data import Batch, Data
+from .calorimeter import calorimeter
--- caloutils/calorimeter.py
+++ caloutils/calorimeter.py
@@ -1,12 +1,15 @@
 class Calorimeter:
     def __init__(self) -> None:
-        pass
+        self._ds2 = (45, 16, 9)
+        self._ds3 = (45, 50, 18)
 
-    def set_layout_calochallange_ds2():
-        raise NotImplementedError
+    def set_layout_calochallange_ds2(self):
+        self.num_z, self.num_alpha, self.num_r = self._ds2
+        self.dims = self._ds2
 
-    def set_layout_calochallange_ds3():
-        raise NotImplementedError
+    def set_layout_calochallange_ds3(self):
+        self.num_z, self.num_alpha, self.num_r = self._ds3
+        self.dims = self._ds3
 
 
 calorimeter = Calorimeter()
