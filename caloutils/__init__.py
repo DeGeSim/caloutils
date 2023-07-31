4c6e4a02c7dafa3b39f77eb9535adac2324055c7
mova <mova@users.noreply.github.com>
2023-07-31 18:18:29 +0200

    init calorimeter function

--- caloutils/__init__.py
+++ caloutils/__init__.py
@@ -4,8 +4,27 @@ __author__ = """mova"""
 __email__ = "mova@users.noreply.github.com"
 __version__ = '0.0.9'  # fmt: skip
 
-# from processing.batch_to_Exyz import batch_to_Exyz
-# from processing.pca import fpc_from_batch
-# from processing.voxelize import sum_duplicate_hits, voxelize
-# from torch_geometric.data import Batch, Data
 from .calorimeter import calorimeter
+
+
+def init_calorimeter(caloname: str):
+    """The function `init_calorimeter` initializes a calorimeter geometry.
+    Currently implemented are dataset 2 and 3 of the CaloChallenge:
+    https://github.com/CaloChallenge/homepage
+
+    Parameters
+    ----------
+    caloname : str
+        The parameter `caloname` is a string that represents the name of the calorimeter. It can have two
+    possible values: "ccds2" or "ccds3".
+
+    """
+    match caloname:
+        case "cc_ds2":
+            calorimeter.set_layout_calochallange_ds2()
+        case "cc_ds3":
+            calorimeter.set_layout_calochallange_ds3()
+        case _:
+            raise NotImplementedError(
+                f"No such calorimeter: {caloname}. Options are :'cc_ds2','cc_ds3'"
+            )
--- caloutils/calorimeter.py
+++ caloutils/calorimeter.py
@@ -5,18 +5,48 @@ class Calorimeter:
     def __init__(self) -> None:
         self._ds2 = (45, 16, 9)
         self._ds3 = (45, 50, 18)
-        self.dims: Optional[tuple[int, int, int]] = None
-        self.num_z: Optional[int] = None
-        self.num_alpha: Optional[int] = None
-        self.num_r: Optional[int] = None
+        self._dims: Optional[tuple[int, int, int]] = None
+        self._num_r: Optional[int] = None
+        self._num_alpha: Optional[int] = None
+        self._num_z: Optional[int] = None
+        self._caloname: Optional[str] = None
+
+    def _assert_calo_init(self):
+        if self._caloname is None:
+            raise Exception(
+                "Calorimeter is not initalized. Use"
+                " `init_calorimeter(caloname)` before accassing the attributes."
+            )
+
+    @property
+    def num_r(self):
+        self._assert_calo_init()
+        return self._num_r
+
+    @property
+    def alppha(self):
+        self._assert_calo_init()
+        return self._num_r
+
+    @property
+    def z(self):
+        self._assert_calo_init()
+        return self._num_r
+
+    @property
+    def dims(self):
+        self._assert_calo_init()
+        return self._num_r
 
     def set_layout_calochallange_ds2(self):
-        self.num_z, self.num_alpha, self.num_r = self._ds2
-        self.dims = self._ds2
+        self._num_z, self._num_alpha, self._num_r = self._ds2
+        self._dims = self._ds2
+        self._caloname = "cc_ds2"
 
     def set_layout_calochallange_ds3(self):
-        self.num_z, self.num_alpha, self.num_r = self._ds3
-        self.dims = self._ds3
+        self._num_z, self._num_alpha, self._num_r = self._ds3
+        self._dims = self._ds3
+        self._caloname = "cc_ds3"
 
 
 calorimeter = Calorimeter()
