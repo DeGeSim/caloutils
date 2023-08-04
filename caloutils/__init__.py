8cb8c140c45624fb6e3817121fa16e412783f71c
mova <mova@users.noreply.github.com>
2023-08-04 12:30:43 +0200

    Bump version: 0.0.9 → 0.0.10

--- caloutils/__init__.py
+++ caloutils/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """mova"""
 __email__ = "mova@users.noreply.github.com"
-__version__ = '0.0.9'  # fmt: skip
+__version__ = '0.0.10'  # fmt: skip
 
 from .calorimeter import calorimeter
 
