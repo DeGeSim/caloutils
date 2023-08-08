06ae67157ea68fa4f3069229872c52d3a6aa81b5
mova <mova@users.noreply.github.com>
2023-08-08 09:12:55 +0200

    fix defaults and docs for calc_vars

--- caloutils/variables/calculate_variables.py
+++ caloutils/variables/calculate_variables.py
@@ -1,7 +1,11 @@
 from torch_geometric.data import Batch
 
+from ..processing import (
+    shift_multi_hits,
+    shift_sum_duplicate_hits,
+    sum_duplicate_hits,
+)
 from ..processing.batch_to_Exyz import batch_to_Exyz
-from ..processing.utils import sum_duplicate_hits
 from .analyze_layers import analyze_layers
 from .energy_ratios import cyratio, sphereratio
 from .first_principal_components import first_principal_components
@@ -10,7 +14,8 @@ from .response import response
 
 def calc_vars(
     batch: Batch,
-    sum_multihits: bool = False,
+    sum_multihits: bool = True,
+    shift_multihits: bool = False,
     vars: list = [
         "voxel",
         "sphereratio",
@@ -32,7 +37,9 @@ def calc_vars(
         A Batch object from the PyTorch Geometric library that contains the point cloud
         representation of the events.
     sum_multihits : bool, optional
-        If True, aggregates the energies of hits in the same cell for each event. Defaults to False.
+        If True, aggregates the energies of hits in the same cell for each event. Defaults to True.
+    shift_multihits : bool, optional
+        If True, tries to shift hits that occupy the same cell to neihgboring, empty cells. Defaults to False.
     vars : list of str, optional
         List of variables to calculate for the events in the batch. Defaults to all
         supported variables: ["voxel","sphereratio","cyratio","fpc","showershape","response"].
@@ -42,8 +49,13 @@ def calc_vars(
     Batch
         The input Batch object with calculated variables added.
     """
-    if sum_multihits:
+    if sum_multihits and shift_multihits:
+        batch = shift_sum_duplicate_hits(batch)
+    elif sum_multihits:
         batch = sum_duplicate_hits(batch)
+    elif shift_multihits:
+        batch = shift_multi_hits(batch)
+
     batch = batch_to_Exyz(batch)
     if "sphereratio" in vars:
         batch["sphereratio"] = sphereratio(batch)
